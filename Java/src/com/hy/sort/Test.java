5e2791e2c89808778326d498b0e689c52e78d62c
huangyong <756655135@qq.com>
2017-06-12 14:57:22 +0800

    常用排序算法
    常用的一些排序算法，包括快速排序与归并排序的递归和非递归实现

---
+++ Java/src/com/hy/sort/Test.java
@@ -0,0 +1,18 @@
+package com.hy.sort;
+
+/**
+ * Created by huangyong on 2017/6/5.
+ * 排序测试代码
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = {79, 61, 90, 12, 54, 25, 48, 36, 87, 10, 7, 2, 8, 102, 56};
+        SortFun.quickSort(arr);
+
+        System.out.print("排序后结果：");
+        for (int anArr : arr) {
+            System.out.print(" " + anArr);
+        }
+        System.out.println();
+    }
+}
