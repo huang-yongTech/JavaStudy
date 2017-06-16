e51d61afa2e1fc649f511d95301432b366e9b391
huangyong <756655135@qq.com>
2017-06-16 12:08:16 +0800

    重构路径

---
+++ src/main/java/com/hy/Person.java
@@ -0,0 +1,46 @@
+package com.hy;
+
+/**
+ * Created by huangyong on 2017/6/13.
+ * 测试用
+ */
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
