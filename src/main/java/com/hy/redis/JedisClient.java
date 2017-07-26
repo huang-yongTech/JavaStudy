d4f750d64c09100755773085dfbc531035abcd0c
huangyong <756655135@qq.com>
2017-07-26 11:34:40 +0800

    redis的一些更改

--- src/main/java/com/hy/redis/JedisClient.java
+++ src/main/java/com/hy/redis/JedisClient.java
@@ -10,20 +10,6 @@ public class JedisClient {
 
     public static void main(String[] args) {
         Jedis jedis = JedisUtil.getJedis();
-        jedis.watch("k1");
-        int k1 = Integer.parseInt(jedis.get("k1"));
-        try {
-            Thread.sleep(5000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (k1 < 10) {
-            jedis.unwatch();
-            System.out.println("已被其他客户端修改");
-            System.out.println(k1);
-        }else {
-            System.out.println("未被修改");
-            System.out.println(k1);
-        }
+        jedis.set("k1", "测试");
     }
 }
