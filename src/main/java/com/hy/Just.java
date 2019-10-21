688a1f1f74ce32133e5736a379671dd9c3be3359
huangyong <756655135@qq.com>
2019-10-21 14:05:45 +0800

    TreeSet逆序

--- src/main/java/com/hy/Just.java
+++ src/main/java/com/hy/Just.java
@@ -1,6 +1,8 @@
 package com.hy;
 
-import com.hy.utils.LogUtil;
+
+import java.util.Comparator;
+import java.util.TreeSet;
 
 /**
  * HashMap的无序是指数据存入和取出来的顺序不一样，但是HashMap的数据取出还是按照一定顺序的
@@ -8,52 +10,23 @@ import com.hy.utils.LogUtil;
 public class Just {
 
     public static void main(String[] args) {
-        LogUtil.getLogger().info("Info测试");
-        System.out.println(test());
-    }
-
-    public int factorial(int n) {
-        if (n < 0)
-            throw new RuntimeException("负数没有阶乘");
-        if (n <= 1)
-            return 1;
-        else {
-            int sum = 1;
-            for (int i = 2; i <= n; i++) {
-                sum *= i;
+        TreeSet<Integer> treeSet = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
             }
-            return sum;
-        }
-    }
+        });
 
-    /**
-     * 斐波那契序列
-     *
-     * @param n 待计算数字
-     * @return 斐波那契数
-     */
-    public int fibonacci(int n) {
-        if (n <= 0)
-            throw new RuntimeException("斐波那契序列从第一位开始");
-        else if (n == 1) {
-            return 0;
-        } else if (n == 2) {
-            return 1;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-    }
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(5);
+        treeSet.add(6);
+        treeSet.add(7);
 
-    private static int test() {
-        int a = 1;
-        try {
-            return a;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            a = 2;
+        for (Integer integer : treeSet) {
+            System.out.println(integer);
         }
-        System.out.println("执行到2");
-        return a;
     }
 }
